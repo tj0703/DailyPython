1e352e934bc90b6d98cfb06557e2e0870b63b5b4
tj0703 <tanvi.jain@aalto.fi>
2021-07-01 09:48:56 +0300

    more changes

--- PycharmProjects/day-18-start/main.py
+++ PycharmProjects/day-18-start/main.py
@@ -1,3 +1,24 @@
-from turtle import Turtle
+from turtle import *
 
 timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("black")
+# forward(1)
+# left()
+# forward(300)
+color('red', 'yellow')
+begin_fill()
+while True:
+    for i in range(1):
+        penup()
+        forward(10)
+        pendown()
+        forward(10)
+    left(10)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
+screen = Screen()
+screen.exitonclick()
