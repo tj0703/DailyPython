6e440c01e7515a807ef17374643b395f22e08f7f
tj0703 <tanvi.jain@aalto.fi>
2021-07-06 16:36:42 +0300

    new views added

--- Downloads/quiz-game-start/myquiz/quiz/views.py
+++ Downloads/quiz-game-start/myquiz/quiz/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
+    context = {'latest_question_list': latest_question_list, }
     return render(request, 'quiz/index.html', context)
 
 
@@ -15,9 +17,27 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'quiz/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'quiz/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # using F() to avoid race condition which may occur while updating
+        # model variable 'vote' by two different users
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        selected_choice.refresh_from_db()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('quiz:results', args=(question.id,)))
